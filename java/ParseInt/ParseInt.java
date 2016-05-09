0c1d2ec4ffbffe402279be8da657d8a718973098
furandon-pig <furandon.pig@gmail.com>
2016-05-10 02:46:44 +0900

    Added Integer.{toString,parseInt} sample in Java 	new file:   java/ParseInt/ParseInt.java

---
+++ java/ParseInt/ParseInt.java
@@ -0,0 +1,9 @@
+/* ParseInt.java */
+
+public class ParseInt {
+    public static void main(String... args) {
+        System.out.println(Integer.toString(123));
+        System.out.println(Integer.parseInt("456"));
+    }
+}
+
