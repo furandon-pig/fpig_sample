442a9357ae2f1c0a8e97b74a80bf1b5158ba7300
furandon-pig <furandon.pig@gmail.com>
2016-05-13 01:51:07 +0900

    Added AngularJS application template 	new file:   js/angular_app_skeletonn/app.js 	new file:   js/angular_app_skeletonn/index.html

---
+++ js/angular_app_skeletonn/app.js
@@ -0,0 +1,37 @@
+/* app.js */
+
+var app = angular.module('App', []);
+
+app.controller('AppController', ['$scope',
+      function($scope) {
+        var idx = 1;
+
+        // initialize
+        $scope.init = function() {
+          $scope.message = "Hello,World.";
+
+          $scope.items = [
+            { id: idx++, name: "apple"  },
+            { id: idx++, name: "grape"  },
+            { id: idx++, name: "orange" },
+          ];
+        }
+
+        $scope.addItem = function() {
+          $scope.items.push({ id: idx, name: "item(" + idx + ")" });
+          idx++;
+        }
+
+        $scope.delItem = function(item) {
+          for (var i in $scope.items) {
+            if ($scope.items[i].id == item.id) {
+              $scope.items.splice(i, 1);
+              break;
+            }
+          }
+        }
+
+        $scope.init();
+      }
+]);
+
