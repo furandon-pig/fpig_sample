9cc5f639bfa661e1b5feabf92715205a86a83fe6
furandon-pig <furandon.pig@gmail.com>
2016-05-16 01:39:34 +0900

    Fixed indentation 	modified:   csharp/EstimateTime/EstimateTime.cs

--- csharp/EstimateTime/EstimateTime.cs
+++ csharp/EstimateTime/EstimateTime.cs
@@ -10,38 +10,38 @@ namespace FpigSample
     {
       //var watch = new System.Diagnostics.Stopwatch();
       var watch = new Stopwatch();
-  		var max = 100000;
+      var max = 100000;
   
-  		watch.Start();
-  		for (var i = 0; i < max; i++) {
+      watch.Start();
+      for (var i = 0; i < max; i++) {
         try
-  			{
+        {
           var v = int.Parse(str);
-					v++;  // XXX
-  			}
-  			catch
-  			{
+          v++;  // XXX
+        }
+        catch
+        {
           // do nothing
-  			}
-  		}
-  		watch.Stop();
+        }
+      }
+      watch.Stop();
   
-  		var result  = watch.ElapsedMilliseconds;
-  		var avg = (float)result / max;
+      var result  = watch.ElapsedMilliseconds;
+      var avg = (float)result / max;
   
-  		System.Console.WriteLine("avg= {0}", avg);
+      System.Console.WriteLine("avg= {0}", avg);
     }
 
     public static void Main()
     {
       var et = new EstimateTime();
 
-			System.Console.WriteLine("-> parse '10'");
-			et.Estimate("10");
+      System.Console.WriteLine("-> parse '10'");
+      et.Estimate("10");
 
-			System.Console.WriteLine("-> parse null (trap exception)");
-			et.Estimate(null);
+      System.Console.WriteLine("-> parse null (trap exception)");
+      et.Estimate(null);
     }
-	}
+  }
 }
 
