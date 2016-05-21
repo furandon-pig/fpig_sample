6bb363b8539a8c75b7aec9bae7386c15cc0309cc
furandon-pig <furandon.pig@gmail.com>
2016-05-21 21:31:04 +0900

    Added Python sample 	new file:   python/hello/hello.py

---
+++ python/hello/hello.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+import codecs
+
+#sys.stdin  = codecs.getreader('euc_jp')(sys.stdin)
+#sys.stdout = codecs.getwriter('utf-8')(sys.stdout)
+
+print 'hello,world'
+
