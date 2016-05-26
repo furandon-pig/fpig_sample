516172adc98e31f7f24a18f84c9f2e0be49c68a7
furandon-pig <furandon.pig@gmail.com>
2016-05-26 23:17:48 +0900

    Added Tuple sample in C# 	new file:   csharp/TupleSample/TupleSample.cs

---
+++ csharp/TupleSample/TupleSample.cs
@@ -0,0 +1,24 @@
+/* TupleSample.cs */
+
+/*
+ * Tupleクラス
+ * https://msdn.microsoft.com/ja-jp/library/system.tuple(v=vs.110).aspx
+ */
+
+using System;
+
+namespace FpigSample
+{
+  public class TupleSample
+  {
+    public static void Main()
+    {
+      var Id = 1;
+      var myTuple = Tuple.Create(Id++, "hello", DateTime.Now);
+      Console.WriteLine(myTuple.Item1);
+      Console.WriteLine(myTuple.Item2);
+      Console.WriteLine(myTuple.Item3);
+    }
+  }
+}
+
