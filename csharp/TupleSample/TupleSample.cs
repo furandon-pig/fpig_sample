df86ccfc1dd12cb9177a6ee6834ff661a6af80ea
furandon-pig <furandon.pig@gmail.com>
2016-05-27 08:21:02 +0900

    Modified Tuple sample 	modified:   csharp/TupleSample/TupleSample.cs

--- csharp/TupleSample/TupleSample.cs
+++ csharp/TupleSample/TupleSample.cs
@@ -15,9 +15,14 @@ namespace FpigSample
     {
       var Id = 1;
       var myTuple = Tuple.Create(Id++, "hello", DateTime.Now);
+
       Console.WriteLine(myTuple.Item1);
       Console.WriteLine(myTuple.Item2);
       Console.WriteLine(myTuple.Item3);
+
+      Console.WriteLine(myTuple.Item1.GetType());
+      Console.WriteLine(myTuple.Item2.GetType());
+      Console.WriteLine(myTuple.Item3.GetType());
     }
   }
 }
